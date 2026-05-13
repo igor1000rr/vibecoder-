4e4d45742be07d7b856acd6ce6b5eff4f306f993
PHPLIFE <igorsuhockii@gmail.com>
2026-05-14 01:43:38 +0300

    extensions/git/ipc/ipcClient.ts: fix Buffer.concat variance.
    Buffer.concat(chunks) → Buffer.concat(chunks as readonly Uint8Array[]).

--- extensions/git/src/ipc/ipcClient.ts
+++ extensions/git/src/ipc/ipcClient.ts
@@ -34,7 +34,8 @@ export class IPCClient {
 
 				const chunks: Buffer[] = [];
 				res.on('data', d => chunks.push(d));
-				res.on('end', () => c(JSON.parse(Buffer.concat(chunks).toString('utf8'))));
+				// Cast обходит Buffer[]/Uint8Array<ArrayBufferLike>[] variance issue в новых @types/node.
+				res.on('end', () => c(JSON.parse(Buffer.concat(chunks as readonly Uint8Array[]).toString('utf8'))));
 			});
 
 			req.on('error', err => e(err));
