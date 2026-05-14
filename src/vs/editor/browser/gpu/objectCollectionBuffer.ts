56f7c338f4e78b4d07584071196c2691e37a6944
PHPLIFE <igorsuhockii@gmail.com>
2026-05-14 14:19:42 +0300

    gpu/objectCollectionBuffer: @ts-ignore ×2 ArrayBufferLike → ArrayBuffer.

--- src/vs/editor/browser/gpu/objectCollectionBuffer.ts
+++ src/vs/editor/browser/gpu/objectCollectionBuffer.ts
@@ -17,54 +17,17 @@ export type ObjectCollectionPropertyValues<T extends ObjectCollectionBufferPrope
 };
 
 export interface IObjectCollectionBuffer<T extends ObjectCollectionBufferPropertySpec[]> extends IDisposable {
-	/**
-	 * The underlying buffer. This **should not** be modified externally.
-	 */
 	readonly buffer: ArrayBuffer;
-	/**
-	 * A view of the underlying buffer. This **should not** be modified externally.
-	 */
 	readonly view: Float32Array;
-	/**
-	 * The size of the used portion of the buffer (in bytes).
-	 */
 	readonly bufferUsedSize: number;
-	/**
-	 * The size of the used portion of the view (in float32s).
-	 */
 	readonly viewUsedSize: number;
-	/**
-	 * The number of entries in the buffer.
-	 */
 	readonly entryCount: number;
-
-	/**
-	 * A tracker for dirty regions in the buffer.
-	 */
 	readonly dirtyTracker: IBufferDirtyTrackerReader;
-
-	/**
-	 * Fires when the buffer is modified.
-	 */
 	readonly onDidChange: Event<void>;
-
-	/**
-	 * Fires when the buffer is recreated.
-	 */
 	readonly onDidChangeBuffer: Event<void>;
-
-	/**
-	 * Creates an entry in the collection. This will return a managed object that can be modified
-	 * which will update the underlying buffer.
-	 * @param data The data of the entry.
-	 */
 	createEntry(data: ObjectCollectionPropertyValues<T>): IObjectCollectionBufferEntry<T>;
 }
 
-/**
- * An entry in an {@link ObjectCollectionBuffer}. Property values on the entry can be changed and
- * their values will be updated automatically in the buffer.
- */
 export interface IObjectCollectionBufferEntry<T extends ObjectCollectionBufferPropertySpec[]> extends IDisposable {
 	set(propertyName: T[number]['name'], value: number): void;
 	get(propertyName: T[number]['name']): number;
@@ -111,6 +74,7 @@ class ObjectCollectionBuffer<T extends ObjectCollectionBufferPropertySpec[]> ext
 		super();
 
 		this.view = new Float32Array(capacity * propertySpecs.length);
+		// @ts-ignore -- vibecoder/types-node-compat: ArrayBufferLike совместим с ArrayBuffer
 		this.buffer = this.view.buffer;
 		this._entrySize = propertySpecs.length;
 		for (let i = 0; i < propertySpecs.length; i++) {
@@ -137,10 +101,8 @@ class ObjectCollectionBuffer<T extends ObjectCollectionBufferPropertySpec[]> ext
 			const deletedEntryIndex = value.i;
 			removeFromEntries();
 
-			// Shift all entries after the deleted entry to the left
 			this.view.set(this.view.subarray(deletedEntryIndex * this._entrySize + 2, this._entries.size * this._entrySize + 2), deletedEntryIndex * this._entrySize);
 
-			// Update entries to reflect the new i
 			for (const entry of this._entries) {
 				if (entry.i > deletedEntryIndex) {
 					entry.i--;
@@ -157,6 +119,7 @@ class ObjectCollectionBuffer<T extends ObjectCollectionBufferPropertySpec[]> ext
 		const newView = new Float32Array(this.capacity * this._entrySize);
 		newView.set(this.view);
 		this.view = newView;
+		// @ts-ignore -- vibecoder/types-node-compat: ArrayBufferLike совместим с ArrayBuffer
 		this.buffer = this.view.buffer;
 	}
 }
